95f3e13ba64184cb4e8be865b92734c4446f0822
mlammers <Maikel.Lammers@experteer.com>
2013-07-13 15:30:10 +0200

    Tunnel Messages

--- lib/gnunet/services/mesh/tunnel_msg_base.rb
+++ lib/gnunet/services/mesh/tunnel_msg_base.rb
@@ -4,3 +4,37 @@
 #both ways
 
 
+module Gnunet
+  module Services
+    module Mesh
+      class TunnelMsgBase < ::Gnunet::GnuMessage
+
+
+        add_field :tunnel_id, :u32
+        add_field :peer,:string,:length => 512/8
+        add_field :port, :u32
+        add_field :options, :u32
+
+        # Type for tunnel numbering.
+        # - Local tunnel numbers given by the service (incoming) are >= 0xB0000000
+        # - Local tunnel numbers given by the client (created) are >= 0x80000000
+        # - Global tunnel numbers are < 0x80000000
+        attr_accessor :tunnel_id #32 bit
+
+        attr_accessor :peer #512 bit
+        attr_accessor :options #32
+        attr_accessor :port #32
+
+        def initialize(atts={})
+          atts.each_pair do |key,value|
+            send("#{key}=", value)
+          end
+        end
+
+        def size
+          80
+        end
+      end
+    end
+  end
+end
--- lib/gnunet/services/mesh/tunnel_msg_destroy.rb
+++ lib/gnunet/services/mesh/tunnel_msg_destroy.rb
@@ -7,39 +7,10 @@
 module Gnunet
   module Services
     module Mesh
-      class TunnelMsgDelete < TunnelMsgBase
-        #
-        #
-        #add_field :desired_replication_level, :u32
-        #add_field :block_type,:u32
-        #add_field :options,:u32
-        #add_field :unique_id,:u64
-        #add_field :key,:string,:length => 512/8
-        #
-        #attr_accessor :options #32 bit
-        #attr_accessor :desired_replication_level #32 bit
-        #attr_accessor :unique_id #64 bit
-        #attr_accessor :key #512 bit
-        #
-        #def initialize(atts={})
-        #  atts.each_pair do |key,value|
-        #    send("#{key}=", value)
-        #  end
-        #end
-        #
-        #def block_type
-        #  #8 on put = nocheck /0 on get = everything
-        #  0
-        #end
-        #
-        #def size
-        #  84
-        #end
-        #
-        #def type
-        #  #32 bit - 143 for client get - s. /src/include/gnunet_protocols.h
-        #  143
-        #end
+      class TunnelMsgDestroy < TunnelMsgBase
+        def type
+          274
+        end
       end
     end
   end
